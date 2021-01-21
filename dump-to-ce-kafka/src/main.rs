45a0445b4f2067f6cec786dd5dc33760e71cc5c7
slinkydeveloper <francescoguard@gmail.com>
2021-01-21 12:19:49 +0100

    Added console dumper, to test collected files

--- dump-to-ce-kafka/src/main.rs
+++ dump-to-ce-kafka/src/main.rs
@@ -11,6 +11,12 @@ use std::env;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file_location = args.get(1).expect("Input file location");
+    if args.len() == 2 {
+        // Dump to console
+        dump_to_console(input_file_location)
+            .await;
+        return
+    }
     let brokers = args.get(2).expect("Brokers");
     let topic_name = args.get(3).expect("Topic name");
 
@@ -19,7 +25,6 @@ async fn main() {
         .set("message.timeout.ms", "5000")
         .create()
         .expect("Producer creation error");
-
 
     let mut f = File::open(input_file_location)
         .await
@@ -58,6 +63,33 @@ async fn main() {
             )
             .await
             .expect("Correct production of messages");
+
+        inc += 1;
+    }
+}
+
+async fn dump_to_console(input_file_location: &str) {
+    let mut f = File::open(input_file_location)
+        .await
+        .expect("Opened file");
+    let mut buffer = [0; 328];
+
+    let mut inc: u32 = 0;
+
+    loop {
+        let n = f.read(&mut buffer)
+            .await
+            .expect("Read file");
+
+        if n == 0 {
+            println!("EOF, closing");
+            return
+        }
+
+        let lap_time = u32::from_le_bytes([buffer[40], buffer[41], buffer[42], buffer[43]]);
+        let speed = f32::from_le_bytes([buffer[8], buffer[9], buffer[10], buffer[11]]);
+
+        println!("{}: Lap time '{}', Speed '{}'", inc, lap_time, speed);
 
         inc += 1;
     }
